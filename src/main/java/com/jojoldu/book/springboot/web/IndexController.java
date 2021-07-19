9e59715aee3e88916066d1e1c7c65d971f1fe3b0
bominjang <qhdud9443@naver.com>
2021-07-19 17:16:13 +0900

    feat : index.mustache, IndexController, IndexControllerTest
    - 기본페이지 생성
    - IndexController 에서의 url mapping
    - URL 호출 시 페이지의 내용이 제대로 호출되는 지에 대한 테스트

---
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+        // Controller에서 문자열을 반환할 때, mustache starter가 앞에는 경로, 뒤에는 mustache 확장자를 붙여줌.
+        //그러면 ViewResolver가 URL 요청의 결과를 전달할 타입과 값을 지정하면서 처리.
+    }
+}
