cfb91c004200d0431536e9067e64933275f219cb
bominjang <qhdud9443@naver.com>
2021-07-21 16:38:23 +0900

    fixed untracked files

--- src/main/java/com/jojoldu/book/springboot/domain/user/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jojoldu.book.springboot.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email); //이미 생성된 사용자인지 체크기위한 메서드
-}
